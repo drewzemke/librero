ffa83452787679dbb0147bdc62f72a18b8058aaf
Drew Zemke <drew.zemke@gmail.com>
2024-11-12 23:08:00 -0800

    feat(search): show loading message, only request required info

--- src/app/home.rs
+++ src/app/home.rs
@@ -1,4 +1,4 @@
-use leptos::{prelude::*, task::spawn_local};
+use leptos::{either::Either, prelude::*};
 use leptos_use::signal_debounced;
 use reqwest::Url;
 use serde::{Deserialize, Serialize};
@@ -23,19 +23,27 @@ struct OpenLibrarySearchResult {
 
 const SEARCH_URL_BASE: &'static str = "https://openlibrary.org/search.json";
 
-async fn search_books(search: String) -> Result<Vec<OpenLibraryBook>, reqwest::Error> {
+// HACK: replaced reqwest::Error with String because the former is not Clone
+async fn search_books(search: String) -> Result<Vec<OpenLibraryBook>, String> {
+    if search.is_empty() {
+        return Ok(vec![]);
+    }
+
     let url = Url::parse(&SEARCH_URL_BASE)
         .expect("base url should be parseable")
         .query_pairs_mut()
         .append_pair("q", &search)
+        .append_pair("fields", "title")
         .append_pair("limit", "5")
         .finish()
         .clone();
 
     let books = reqwest::get(url)
-        .await?
+        .await
+        .map_err(|_| String::from("Something went wrong."))?
         .json::<OpenLibrarySearchResult>()
-        .await?
+        .await
+        .map_err(|_| String::from("Something went wrong."))?
         .docs;
 
     Ok(books)
@@ -46,28 +54,41 @@ pub fn BookSearch() -> impl IntoView {
     let (search, set_search) = signal(String::new());
     let debounced_search: Signal<String> = signal_debounced(search, 500.0);
 
-    let (results, set_results) = signal(Vec::new());
+    let results = LocalResource::new(move || search_books(debounced_search.get()));
 
-    Effect::new(move |_| {
-        let term = debounced_search.get();
-        if term.is_empty() {
-            set_results(Vec::new());
-        } else {
-            spawn_local(async move {
-                // TODO: error handling?
-                if let Ok(results) = search_books(term).await {
-                    set_results(results);
+    // FIXME: why can't the return typ be `Suspend<Result<...>>`?
+    // (that won't work here for some reason)
+    let book_list = move || {
+        Suspend::new(async move {
+            match results.await {
+                Ok(books) => {
+                    if books.is_empty() {
+                        Either::Left(view! { <p>{"Nothing found."}</p> })
+                    } else {
+                        Either::Right(
+                            books
+                                .iter()
+                                .map(move |book| {
+                                    view! { <li>{book.title.clone()}</li> }
+                                })
+                                .collect::<Vec<_>>(),
+                        )
+                    }
                 }
-            })
-        }
-    });
+                .into_any(),
+                Err(err) => view! { <p>{err}</p> }.into_any(),
+            }
+        })
+    };
 
     view! {
         <input bind:value=(search, set_search) />
-        <ul>
-            <For each=results key=|book| book.title.clone() let:book>
-                <li>{book.title}</li>
-            </For>
-        </ul>
+        <Show when=move || !debounced_search().is_empty()>
+            <Suspense fallback=move || {
+                view! { <p>"Searching..."</p> }
+            }>
+                <ul>{book_list}</ul>
+            </Suspense>
+        </Show>
     }
 }
